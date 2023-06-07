885932497082c5f1d03b719d93159446ccbf58c1
NataA <nyabramova@gmail.com>
2023-06-07 21:12:50 +0200

    Miles calculation

--- src/Main.java
+++ src/Main.java
@@ -2,3 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
+        int price = 25354; // Стоимость билета
+        int single = 20; // Количество рублей за одну милю
+
+        System.out.println("Количество миль, начисленных за билет:");
+        System.out.println((price / single) + " миль(-я)");
+
+    }
+}
